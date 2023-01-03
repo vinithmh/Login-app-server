b371178704f2fda3425cb4a60f111f591d794ab4
Vinith M H <vinithmh2000@gmail.com>
2023-01-03 18:58:47 +0530

    Ports addded

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const routerUrls = require("./routers/routes");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const port = process.env.PORT || 5000;
 
 dotenv.config();
 
@@ -16,4 +17,4 @@ mongoose.connect(process.env.DATABASE_ACCESS, () =>
 app.use(express.json());
 app.use(cors());
 app.use("/", routerUrls);
-app.listen(5000, () => console.log("Server is running"));
+app.listen(port, () => console.log("Server is running"));
